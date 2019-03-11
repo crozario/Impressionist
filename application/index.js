1bd90eabc73fadbf7f1ed65ebdef8346f3a8655f
Crossley Rozario <crozariodev@gmail.com>
2019-03-11 19:29:23 -0400

    added createbutton again

--- application/index.js
+++ application/index.js
@@ -2,15 +2,30 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-app.get('/', function(request, response) {
-    return response.send("hello world");
+
+// app.get('/', function(request, response) {
+//     return response.send("hello world");
+// })
+
+// client connected to server
+io.on('connection', socket => {
+    console.log('a user has connected');
+
+
+    // client disconnected
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected');
+    });
+    
+    // test message
+    socket.on('audio', msg => {
+        console.log("got blog data");
+    })
+
 })
 
 
-io.on('connection', function(data){
-    console.log('connection');
-     
+// server.listen(80);
+server.listen(80, () => {
+    console.log("listening on port 80");
 })
-
-
-server.listen(3000);
--- frontend/media.js
+++ frontend/media.js
@@ -1,4 +1,6 @@
-import * as io from 'socket.io-client';
+var socket = io.connect('http://localhost:80');
+
+socket.emit('hello message', "hello i'm a user");
 
 window.onload = () => {
     let video = document.createElement('video');
@@ -25,68 +27,89 @@ window.onload = () => {
         console.log("video play pressed");
     });
 
+    // var socket = io.connect('http://localhost:80');
+
+    // socket.emit('hello message', "hello i'm a user");
+
+    getAudioData();
 
 };
 
 
+let createButton = (context, value, onclick) => {
+    let button = document.createElement('input');
+    button.type = "button";
+    button.value = value;
+    button.onclick = onclick;
+    context.appendChild(button);
+}
 
-if (navigator.mediaDevices) {
-    console.log('getUserMedia supported.');
-    var constraints = { audio: true };
-    var chunks = [];
+let getAudioData = () => {
+    if (navigator.mediaDevices) {
+        console.log('getUserMedia supported.');
+        var constraints = { audio: true };
+        var chunks = [];
+    
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+    
+            var mediaRecorder = new MediaRecorder(stream);
+    
+            var videoElement = document.getElementById("video-content");
+    
+            videoElement.onplay = () => {
+                mediaRecorder.start();
+                console.log(mediaRecorder.state);
+                console.log("recorder started");
+            }
+    
+            videoElement.onpause = () => {
+                mediaRecorder.stop();
+                console.log(mediaRecorder.state);
+                console.log("recorder ended");
+            }
+    
+            mediaRecorder.onstop = function(e) {
+                const blob = new Blob(chunks, { type: 'audio/webm' });
+                createAudioElement(URL.createObjectURL(blob));
+                
+                // fetch('http://127.0.0.1:5000/', {
+                //   method: "POST",
+                //   mode: "no-cors",
+                //   credentials: "same-origin",
+                //   body: blob
+                // }).then(function(response) {
+                //   if(response.ok) {
+                //     return response.blob();
+                //   }
+                //   throw new Error('Network response was not ok.');
+                // }).then(function(myBlob) {
+                //   var objectURL = URL.createObjectURL(myBlob);
+                //   myImage.src = objectURL;
+                // }).catch(function(error) {
+                //   console.log('There has been a problem with your fetch operation: ', error.message);
+                // });
 
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(stream) {
+                // console.log(chunks);
 
-        var mediaRecorder = new MediaRecorder(stream);
-
-        var videoElement = document.getElementById("video-content");
-
-        videoElement.onplay = () => {
-            mediaRecorder.start();
-            console.log(mediaRecorder.state);
-            console.log("recorder started");
-        }
-
-        videoElement.onpause = () => {
-            mediaRecorder.stop();
-            console.log(mediaRecorder.state);
-            console.log("recorder ended");
-        }
-
-        mediaRecorder.onstop = function(e) {
-            const blob = new Blob(chunks, { type: 'audio/webm' });
-            createAudioElement(URL.createObjectURL(blob));
-
-            // fetch('http://127.0.0.1:5000/', {
-            //   method: "POST",
-            //   mode: "no-cors",
-            //   credentials: "same-origin",
-            //   body: blob
-            // }).then(function(response) {
-            //   if(response.ok) {
-            //     return response.blob();
-            //   }
-            //   throw new Error('Network response was not ok.');
-            // }).then(function(myBlob) {
-            //   var objectURL = URL.createObjectURL(myBlob);
-            //   myImage.src = objectURL;
-            // }).catch(function(error) {
-            //   console.log('There has been a problem with your fetch operation: ', error.message);
-            // });
+                
+                socket.emit("audio", blob);
+    
+            }
+    
+            mediaRecorder.ondataavailable = function(e) {
+                console.log(e);
+                chunks.push(e.data);
+            }
+    
+        })
+        .catch(function(err) {
+            console.log('The following error occurred: ' + err);
+        })
+    };
+} 
 
 
-        }
-
-        mediaRecorder.ondataavailable = function(e) {
-            chunks.push(e.data);
-        }
-
-    })
-    .catch(function(err) {
-        console.log('The following error occurred: ' + err);
-    })
-};
 
 
 function createAudioElement(blobUrl) {
