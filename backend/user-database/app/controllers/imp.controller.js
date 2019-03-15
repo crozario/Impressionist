b162a69625b7771dc193ae63d5533711bc0d4fe6
Deborah Santo <deb.santo@comcast.net>
2019-03-15 18:46:00 -0400

    updated return messages
    Updated return messages. 
    
    status: "success" || "failure"
    error: (if failure)
    info: (when success and data needs to be sent back)

--- backend/user-database/app/controllers/imp.controller.js
+++ backend/user-database/app/controllers/imp.controller.js
@@ -11,27 +11,27 @@ const schema = require('../models/imp.model.js');
 exports.isUniqueUsername = (req,res) => {
 	// validate request
 	if(!req.body.username) {
-		res.setHeader('Execution', 'failure');
 		return res.status(400).json({
-			message: "Need username"
+			status: "failure",
+			error: "a username was not provided"
 		});
 	}
 	schema.User.findOne({'credentials.username': req.body.username})
 		.then(exists => {
-			res.setHeader('Execution', 'success');
 			if(!exists) {
 				return res.json({
-					message: "user-provided username is unique"
+					status: "success"
 				});
 			} else {
 				return res.json({
-					message: "user-provided username already exists"
+					status: "failure",
+					error: "the username provided is already taken"
 				});
 			}
 		}).catch(err => {
-			res.setHeader('Execution', 'failure');
 			return res.status(500).json({
-				message: err.message || "Error retrieving information from the database"
+				status: "failure",
+				error: err.message || "error retrieving information from the database"
 			});
 		});
 };
@@ -39,10 +39,10 @@ exports.isUniqueUsername = (req,res) => {
 // user sign-up, store basic sign-up information 
 exports.signUp = (req,res) => {
 	// validate request
-	if(!req.body.firstName && !req.body.lastName && !req.body.email && !req.body.username && !req.body.password) {
-		res.setHeader('Execution', 'failure');
+	if(!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.username || !req.body.password) {
 		return res.status(400).json({
-			message: "Need user data at sign-up"
+			status: "failure",
+			error: "some or all required fields were not provided"
 		});
 	}
 	// create a new user
@@ -60,12 +60,14 @@ exports.signUp = (req,res) => {
 	// store user information in the database 
 	user.save()
 	.then(data => {
-		res.setHeader('Execution', 'success');
-		res.json(data);
+		return res.json({
+			status: "success",
+			info: data
+		});
 	}).catch(err => {
-		res.setHeader('Execution', 'failure');
-		res.status(500).json({
-			message: err.message || "Some error occured while storing the user's information."
+		return res.status(500).json({
+			status: "failure",
+			error: err.message || "error occured while storing information in the database"
 		});
 	});
 };
@@ -73,10 +75,10 @@ exports.signUp = (req,res) => {
 // user sign-in, verify that user-provided username/email exists and check password
 exports.signIn = (req,res) => {
 	// validate request
-	if(!req.body.email && !req.body.username && !req.body.password) {
-		res.setHeader('Execution', 'failure');
+	if(!req.body.user && !req.body.password) {
 		return res.status(400).json({
-			message: "Need user data at sign-up"
+			status: "failure",
+			error: "username or password was not provided"
 		});
 	}
 	// build query based on whether the user signed-in with email or username
@@ -89,30 +91,30 @@ exports.signIn = (req,res) => {
 	}
 	query.exec(function(err,doc) {
 		if(err) {
-			res.setHeader('Execution','failure');
 			return res.status(500).json({
-				message: err.message || "Some error occured"
+				status: "failure",
+				error: err.message || "error occured when retrieving information from the database"
 			});
 		}
 		// compare user-provided password with the hashed password from db, doc.credentials.password
-		console.log(doc.credentials.password);
-		console.log(req.body.password);
 		bcrypt.compare(req.body.password,doc.credentials.password,function(err,result) {
 			if(err) {
-				res.setHeader('Execution', 'failure');
 				return res.status(500).json({
-					message: err.message || "Some error occured"
+					status: "failure",
+					error: err.message || "error occured when comparing passwords"
 				});
 			}
-			res.setHeader('Execution', 'success');
 			if(result == true) {
 				doc.lastLogin = Date("<YYYY-mm-ddTHH:MM:ss>");
 				doc.save();
-				res.json(result);
+				return res.json({
+					status: "success",
+					info: result
+				});
 			} else {
-				console.log(result);
-				res.json({
-					message: "password does not match; user is not allowed to log-in"
+				return res.json({
+					status: "failure",
+					error: "user-provided password does not match"
 				});
 			}
 		});
