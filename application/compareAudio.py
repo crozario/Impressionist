c0ccffb12e1ba0cf4dc2b6d391ea20a6ab98e73e
shahhaard47 <shaard74@gmail.com>
2019-03-19 22:29:47 -0400

    Can stream audio using websockets. Still need to integrate with application.

--- application/compareAudio.py
+++ application/compareAudio.py
@@ -13,8 +13,23 @@ from databuilder.extractFeatures import extractFeature as extract
 sys.path.insert(0, '../signalComparison/')
 from signalComparison.compareSig import compareSignals as compare
 
-def compareAudioToFeature(verbose=False):
-    audioFile = 'test.wav' 
+def compareAudioToFeature(audioFile, featureFile, verbose=False):
+    assert (".wav" in audioFile or ".webm" in audioFile), "Error: only supports comparing audio for .wav and .webm files."
+        
+    # TODO: convert this step to bash script
+    if audioFile[-4:] == 'webm': # convert to `wav extension`
+        import subprocess
+        newFile = audioFile[:-4] + "wav"
+        cmd = "ffmpeg -y -i " + audioFile + " " + newFile
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        out = result.stdout.decode('utf-8')
+        assert ("command not found" not in out), "Error: could not convert .webm to .wav because FFMPEG program is not installed"
+        assert ("No such file" not in out), "Error: could not find " + audioFile + "."
+        # Not sure if anything else can go wrong
+        #   check successful
+        assert ("size=" in out), "Error: unknown error in converting `.webm` file to `.wav`"
+        audioFile = newFile
+    
     # audioFile = '../frontend/audio_three-dialogue1.wav' # same file yields 100%
     status, error = extract(audioFile, 'test.csv', '../databuilder/configs/prosodyShs.conf', verbose=verbose)
     if not status: #failed
@@ -28,7 +43,10 @@ def compareAudioToFeature(verbose=False):
     return similarity
 
 if __name__=='__main__':
-    similarity = compareAudioToFeature(verbose=False)
+    audioFile = 'test.webm'
+    featureFile = 'audio_three-dialogue1.csv'
+    # print("Running feature comparison...(python)")
+    similarity = compareAudioToFeature(audioFile, featureFile, verbose=False)
     print("---------------------------------------")
     print("Similarity score (out of 100.0):", round(similarity, ndigits=3))
     print("---------------------------------------")
--- application/server.js
+++ application/server.js
@@ -10,7 +10,7 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const fs = require('fs');
-var spawn = require('child_process').spawn
+var spawn = require('child_process').spawn;
 // var toWav = require('audiobuffer-to-wav')
 const WaveFile = require('wavefile');
 
@@ -30,14 +30,14 @@ io.on('connection', socket => {
         console.log("got blob data");
         
         // new audio stream
-        console.log("message.data");
-        console.log(message.data);
-        console.log("inspect message");
+        // console.log("message.data");
+        // console.log(message.data);
+        // console.log("inspect message");
         // console.log(util.inspect(message, false, null, true /* enable colors */));
         let values = new Uint16Array(message.data);
         let buffer = Buffer.from(values);
-        console.log("values uint16array");
-        console.log(values.length);
+        // console.log("values uint16array");
+        // console.log(values.length);
 
         let writeStream = fs.createWriteStream("test.webm");
         writeStream.write(message.data);
@@ -45,21 +45,22 @@ io.on('connection', socket => {
         // FIXME: callback doesn't work
         writeStream.on("finish", () => {
             console.log("wrote data");
-        })
+        });
 
         // What's this for?
-        // writeStream.end(); 
+        writeStream.end(); 
         
         // Execute ffmpeg command here
         // $ ffmpeg -i test.webm test.wav
 
         dataString = '';
         // execute python dataBuilder.extractFeatures.py
-        py = spawn('python3', ['compareAudio.py']);
+        console.log('spawning process');
+        py = spawn('python', ['compareAudio.py']);
 
         // append to dataString from python stdout
         py.stdout.on('data', data =>{
-            dataString += data.toString();
+            dataString += data.toString(); // send back this similarity score
         });
           
         // print dataString after program end
