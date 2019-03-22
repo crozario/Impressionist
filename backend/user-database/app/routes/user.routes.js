f9d29c33367e6baea223c250ea2a47776931adeb
Deborah Santo <deb.santo@comcast.net>
2019-03-21 20:37:35 -0400

    added game initialization

--- backend/user-database/app/routes/user.routes.js
+++ backend/user-database/app/routes/user.routes.js
@@ -7,9 +7,6 @@ module.exports = (app) => {
 	// verify that user-provided username is unique
 	app.get('/user', user.isUniqueUsername);
 
-	// verify that user-provided email is unique
-	app.get('/user', user.isUniqueEmail);
-
 	// user sign-in
 	app.get('/user/signIn', user.signIn);
 
