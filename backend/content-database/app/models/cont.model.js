16c1e6422cc65a0983ab761007f32b31128c62f4
Deborah Santo <deb.santo@comcast.net>
2019-03-22 23:06:33 -0400

    updated the schema for content

--- backend/content-database/app/models/cont.model.js
+++ backend/content-database/app/models/cont.model.js
@@ -1,26 +1,20 @@
 const mongoose = require('mongoose');
 
-const FeatureSchema = mongoose.Schema({
-	numOfDialogues: Number,
-	featureFileLocations: Array
-}, {
-	_id: false
-});
-
 const ContentSchema = mongoose.Schema({
 	mediaType: String,
 	title: String,
 	season: Number,
 	episode: Number,
+	episodeTitle: String,
 	length: Number, // store length of movie/tv show in minutes
 	mediaFileLocation: String,
 	captionFile: String,
-	featureFiles: FeatureSchema
+	featureFiles: Array,
+	emotionsList: Array
 }, {
 	collection: 'content'
 });
 
 module.exports = {
-	Feat: mongoose.model('Feat', FeatureSchema),
 	Cont: mongoose.model('Cont', ContentSchema)
 }
