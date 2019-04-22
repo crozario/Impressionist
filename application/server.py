e91424a858ad1735ee727f45c5fd0df76c9c2c4d
Crossley Rozario <crozariodev@gmail.com>
2019-04-22 16:44:23 -0400

    made debug true

--- application/server.py
+++ application/server.py
@@ -119,6 +119,5 @@ def initializeUserAudioDir():
 if __name__ == '__main__':
     print("Application Server is listening in port " + str(PORT))
     if SAVE_USER_AUDIO: initializeUserAudioDir()
-    app.debug=True
+    app.debug=False
     socketio.run(app, host='0.0.0.0', port=PORT)
-        
