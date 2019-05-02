4dd13c5174e188cbc8523ac55bc4042a1c2b235e
Crossley Rozario <crozariodev@gmail.com>
2019-05-02 12:57:51 -0400

    revised popup

--- frontend/extension/public/scripts/popup.js
+++ frontend/extension/public/scripts/popup.js
@@ -5,12 +5,13 @@ Description: Popup window for chrome extension
 
 */
 
-let userToken = null;
-let userEmail = null;
-let userProfileLink = null;
+// let userToken = null;
+// let userEmail = null;
+// let userProfileLink = null;
 
 let logging = true;
-
+let loggedIn = false
+let email = ""
 
 onload = () => {
     console.log("on popup script");
@@ -18,37 +19,49 @@ onload = () => {
 
     // let searchTab = document.getElementById('search-tab');
     // searchTab.onclick = tabOnClick("search-tab");
-
-    if (isUserLoggedIn() === true) {
-        // Load User Page
-        console.log("Logged In");
-
-        loadUserPage()
-    } else {
-        // login page
-        console.log("Not Logged In");
-        loadLoginPage();
-    }
+    loadSearchPage();
 }
 
 let isUserLoggedIn = () => { 
-
-    return false;
+    
+    return loggedIn;
 }
 
 let setup = () => {
-    let loginButton = document.getElementById('login-button');
-    loginButton.onclick = loginButtonPressed;
+    let loginButtonElement = document.getElementById('login-button');
+    loginButtonElement.onclick = loginButtonPressed;
+
+    let logoutButtonElement = document.getElementById('logout-button')
+    logoutButtonElement.onclick = logoutButtonPressed;
+
+    let leftTabButtonElement = document.getElementById('left-tab')
+    leftTabButtonElement.onclick = leftTabButtonPressed;
+
+    let rightTabButtonElement = document.getElementById('right-tab')
+    rightTabButtonElement.onclick = rightTabButtonPressed;
+
+    let loginTabButtonElement = document.getElementById('login-tab');
+    loginTabButtonElement.onclick = () => {
+        loadLoginSignup("login");
+        return false;
+    }
+
+    let signupTabButtonElement = document.getElementById('signup-tab');
+    signupTabButtonElement.onclick = () => {
+        loadLoginSignup("signup");
+        return false;
+    }
+    
 }
 
-let loadLoginPage = () => {
-    load("login");
+let loadLoginSignupPage = () => {
+    load("login-signup");
 }
 
-let loadSignUpPage = (jsonData) => {
+let loadUserPage = (jsonData) => {
     load("user");
-
-
+    let emailElement = document.getElementById('user-email');
+    emailElement.innerHTML = email;
     // let user_id = jsonData.id;
     // let email = jsonData.email;
     // let emailVerified = jsonData.verified_email // true | false
@@ -74,40 +87,42 @@ let loadSignUpPage = (jsonData) => {
 
 
 let loadSearchPage = () => {
-    load("login");
+    load("search");
 }
 
 let load = (page) => {
-    let loginContainer = document.getElementById('login-container');
-    let signupContainer = document.getElementById('signup-container');
+    let loginSignupContainer = document.getElementById('login-signup-container');
     let userContainer = document.getElementById('user-container');
     let searchContainer = document.getElementById('search-container');
-    let leftTab = document.getElementById('left-tab');
-    let rightTab = document.getElementById('right-tab');
 
-    if(page == "login") {
-        loginContainer.style.display = "block";
-        userContainer.style.display = "none";
-        signupContainer.style.display = "none";
-        searchContainer.style.display = "none";
-        leftTab.onclick = 
-
-    } else if(page == "signup") {
-        signupContainer.style.display = "block";
-        loginContainer.style.display = "none";
+    if(page == "login-signup") {
+        loginSignupContainer.style.display = "block";
+        loadLoginSignup("login")
         userContainer.style.display = "none";
         searchContainer.style.display = "none";
+
     } else if(page == "user") {
         userContainer.style.display = "block";  
-        loginContainer.style.display = "none";
-        signupContainer.style.display = "none";
+        loginSignupContainer.style.display = "none";
         searchContainer.style.display = "none";
 
     } else if(page == "search") {
         searchContainer.style.display = "block";
-        loginContainer.style.display = "none";
+        loginSignupContainer.style.display = "none";
         userContainer.style.display = "none";
-        signupContainer.style.display = "none";
+    }
+}
+
+let loadLoginSignup = (page) => {
+    let loginContainerElement = document.getElementById('login-container');
+    let signupContainerElement = document.getElementById('signup-container');
+
+    if(page === "login") {
+        signupContainerElement.style.display = "none";
+        loginContainerElement.style.display = "block";
+    } else if(page === "signup") {
+        loginContainerElement.style.display = "none"; 
+        signupContainerElement.style.display = "block"; 
     }
 }
 
@@ -115,46 +130,105 @@ let load = (page) => {
 let loginButtonPressed = () => {
     let email = document.getElementById('login-input-email').value;
     let password = document.getElementById('login-input-password').value;
+    
+    email = "wade_owen_watts@email.com"
+    password = "parzival+art3mis"
+    let success = false;
 
-    saveLoginData(email);
-    loadUserPage();
-
-    return
-    let data = {
-        "email" : email,
-        "password" : password
+    if(success === true) {
+        saveLoginData(email)
+        loadUserPage()
+    } else {
+        incorrect_username_or_password_notification()
     }
+    // loggedIn = true;
+    // saveLoginData(email);
+    // loadUserPage();
 
-    let stringifedData = JSON.stringify(data);
+    
+    // return false;
+    // let data = {
+    //     "user" : email,
+    //     "password" : password
+    // }
+    
+    // let stringifedData = JSON.stringify(data);
 
-    var req = new XMLHttpRequest();
+    // var req = new XMLHttpRequest();
 
-    req.onreadystatechange = function () {
-        if (req.readyState == 4) {
-            if (req.status == 200) {  
-                let jsonData = JSON.parse(req.responseText);
-                console.log("LOGIN RESPONSE\n" + jsonData);
-                saveLoginData(email);
-                loadUserPage();
-            } else {
-                alert("error");
-                alert(req.responseText)
-            }
+    // req.onreadystatechange = function () {
+    //     if (req.readyState == 4) {
+    //         if (req.status == 200) {  
+    //             // let jsonData = JSON.parse(req.responseText);
+    //             console.log("LOGIN RESPONSE\n" + req.responseText);
+    //             // saveLoginData(email);
+    //             // loadUserPage();
+    //         } else {
+    //             console.log(req.responseText);
+    //         }
+    //     }
+    // }
+
+    // req.open("POST", "https://impressionist-user-db-api-east-1.crossley.tech/user/signIn", true);
+    // req.setRequestHeader("Content-Type", "application/json");
+    // req.send(stringifedData);
+
+    return false;
+}
+
+let incorrect_username_or_password_notification = () => {
+    document.getElementById("login-form").reset();
+    var status_id = document.getElementById("status");
+    status_id.style.display = "block";
+    status_id.style.opacity = 1;
+    status_id.addEventListener('click', fadeOutEffect);
+    setTimeout(function(){ 
+        status_id.click();
+    }, 1500);
+}
+
+
+let fadeOutEffect = () =>  {
+    var fadeTarget = document.getElementById("status");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
         }
-    }
-
-    req.open("POST", "https://impressionist-user-db-api-east-1.crossley.tech/user/signIn", true);
-    req.setRequestHeader("Content-Type", "application/json");
-    req.send(stringifedData);
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 50); 
 }
 
 let saveLoginData = (email) => {
-    const loggedIn = true
+    loggedIn = true;
+    email = email;
 
 }
 let logoutButtonPressed = () => {
-
+    loggedIn = false;
+    loadLoginSignupPage();
 }
+
+
+let leftTabButtonPressed = () => {
+    loadSearchPage();
+
+    return false;
+}
+
+let rightTabButtonPressed = () => {
+    if(loggedIn === true) {
+        loadUserPage()
+    } else {
+        loadLoginSignupPage()
+    }
+
+    return false;
+}
+
 // let loginButtonPressed = () => {
 
 //     let login_button = document.getElementById('login-button');
